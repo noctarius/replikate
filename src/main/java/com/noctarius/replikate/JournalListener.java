88a0c82286d46075c725263466e3093e2b9595c4
noctarius <me@noctarius.com>
2016-04-25 20:43:19 +0200

    fixed javadoc

--- src/main/java/com/noctarius/replikate/JournalListener.java
+++ src/main/java/com/noctarius/replikate/JournalListener.java
@@ -30,13 +30,13 @@ public interface JournalListener<V> {
     void onFailure(JournalBatch<V> journalBatch, JournalException cause);
 
     /**
-     * This callback method is called when during journal replaying a missing recordId or other suspicious events arise.<br>
-     * The further behavior of the journal replay can be influenced using the callback's result.<br>
+     * <p>This callback method is called when during journal replaying a missing recordId or other suspicious events arise.</p>
+     * The further behavior of the journal replay can be influenced using the callback's result.
      * <ul>
      * <li>{@link ReplayNotificationResult#Continue} means that execution will be move on with the next record or finish
-     * initialization of journal when replay is finished</li><br>
+     * initialization of journal when replay is finished</li>
      * <li>{@link ReplayNotificationResult#Terminate} will silently finish the record reading and will only announce the
-     * already read entries</li><br>
+     * already read entries</li>
      * <li>Finally {@link ReplayNotificationResult#Except} will throw a {@link JournalException} and none of the already
      * read entries will be announced (same can be achieved by throwing an {@link RuntimeException} inside the callback)
      * </li>
@@ -51,15 +51,16 @@ public interface JournalListener<V> {
                                                         JournalRecord<V> currentRecord);
 
     /**
-     * This method is called when during startup of the Journal implementation a previous journal was found and records
-     * replayed. For every record in the existing journal the callback will be executed.<br>
-     * The further behavior of the journal replay can be influenced using the callback's result.<br>
+     * <p>This method is called when during startup of the Journal implementation a previous journal was found and records
+     * replayed. For every record in the existing journal the callback will be executed.</p>
+     * The further behavior of the journal replay can be influenced using the callback's result.
      * <ul>
      * <li>{@link ReplayNotificationResult#Continue} means that execution will be move on with the next record or finish
-     * initialization of journal when replay is finished</li><br>
-     * <li>{@link ReplayNotificationResult#Terminate} will silently finish the record reading</li><br>
+     * initialization of journal when replay is finished</li>
+     * <li>{@link ReplayNotificationResult#Terminate} will silently finish the record reading</li>
      * <li>{@link ReplayNotificationResult#Except} will throw a {@link JournalException} (same can be achieved by
      * throwing an {@link RuntimeException} inside the callback)</li>
+     * </ul>
      *
      * @param journal The journal that is read
      * @param record  The currently read journal record
